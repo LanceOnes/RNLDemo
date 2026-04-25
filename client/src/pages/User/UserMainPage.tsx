58c986de7302e3b6618f837f05bd817438e8a512
LansOnes <johnleonida747@gmail.com>
2026-04-26 01:32:09 +0800

    part 16 userauth done

--- client/src/pages/User/UserMainPage.tsx
+++ client/src/pages/User/UserMainPage.tsx
@@ -8,24 +8,24 @@ import { useRefresh } from "../../hooks/useRefresh";
 import DeleteUserFormModal from "./components/DeleteUserFormModal";
 
 const UserMainPage = () => {
-  const { 
-    isOpen: isAddUserFormModalOpen, 
-    openModal: openAddUserFormModal, 
-    closeModal: closeAddUserFormModal 
+  const {
+    isOpen: isAddUserFormModalOpen,
+    openModal: openAddUserFormModal,
+    closeModal: closeAddUserFormModal
   } = useModal(false);
 
-  const { 
-    isOpen: isEditUserFormModalOpen, 
-    selectedUser: selectedUserForEdit, 
-    openModal: openEditUserFormModal, 
-    closeModal: closeEditUserFormModal 
+  const {
+    isOpen: isEditUserFormModalOpen,
+    selectedUser: selectedUserForEdit,
+    openModal: openEditUserFormModal,
+    closeModal: closeEditUserFormModal
   } = useModal(false);
 
-  const { 
-    isOpen: isDeleteUserFormModalOpen, 
-    selectedUser: selectedUserForDelete, 
-    openModal: openDeleteUserFormModal, 
-    closeModal: closeDeleteUserFormModal 
+  const {
+    isOpen: isDeleteUserFormModalOpen,
+    selectedUser: selectedUserForDelete,
+    openModal: openDeleteUserFormModal,
+    closeModal: closeDeleteUserFormModal
   } = useModal(false);
 
   const {
@@ -33,7 +33,7 @@ const UserMainPage = () => {
     isVisible: toastMessageIsVissible,
     showToastMessage,
     closeToastMessage
-  } = useToastMessage('', false);
+  } = useToastMessage('', false, false);
 
   const { refresh, handleRefresh } = useRefresh(false);
 
@@ -50,23 +50,23 @@ const UserMainPage = () => {
         isOpen={isAddUserFormModalOpen}
         onClose={closeAddUserFormModal}
       />
-      <EditUserFormModal 
-        user={selectedUserForEdit}  
-        onUserUpdated={showToastMessage} 
-        refreshKey={handleRefresh} 
-        isOpen={isEditUserFormModalOpen} 
-        onClose={closeEditUserFormModal} 
+      <EditUserFormModal
+        user={selectedUserForEdit}
+        onUserUpdated={showToastMessage}
+        refreshKey={handleRefresh}
+        isOpen={isEditUserFormModalOpen}
+        onClose={closeEditUserFormModal}
       />
-      <DeleteUserFormModal 
+      <DeleteUserFormModal
         user={selectedUserForDelete}
         onUserDeleted={showToastMessage}
         refreshKey={handleRefresh}
         isOpen={isDeleteUserFormModalOpen}
         onClose={closeDeleteUserFormModal}
       />
-      <UserList 
-        onAddUser={openAddUserFormModal} 
-        onEditUser={(user) => openEditUserFormModal(user)} 
+      <UserList
+        onAddUser={openAddUserFormModal}
+        onEditUser={(user) => openEditUserFormModal(user)}
         onDeleteUser={(user) => openDeleteUserFormModal(user)}
         refreshKey={refresh}
       />
